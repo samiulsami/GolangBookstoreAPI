e1ca3c905c72071fbfcd83a4491cbbb642ad83bb
sami <sami@appscode.com>
2024-03-08 16:52:47 +0600

    added prometheus metrics manually

--- prometheusMetrics/bookMetrics.go
+++ prometheusMetrics/bookMetrics.go
@@ -1,6 +1,12 @@
 package prometheusMetrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func init() {
+	prometheus.MustRegister(BookGetCounter, BookAddCounter, BookDeleteCounter, BookGetAllCounter, BookUpdateCounter)
+}
 
 var BookAddCounter = prometheus.NewCounter(
 	prometheus.CounterOpts{
@@ -9,9 +15,30 @@ var BookAddCounter = prometheus.NewCounter(
 	},
 )
 
-var BookGetounter = prometheus.NewCounter(
+var BookGetCounter = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Name: "book_get_count",
 		Help: "Number of get requests successfully completed",
+	},
+)
+
+var BookGetAllCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "book_get_all_count",
+		Help: "Number of \"get all books\" requests successfully completed",
+	},
+)
+
+var BookDeleteCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "book_delete_count",
+		Help: "Number of delete successfully completed",
+	},
+)
+
+var BookUpdateCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "book_update_count",
+		Help: "Number of update requests successfully completed",
 	},
 )
